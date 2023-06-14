198f2fe238c7c57b3b69fb75e9f555f7e9b23371
XaverWeste <westxave@kkg-do.eu>
2023-06-14 17:44:23 +0200

    recycling of XJLN-Compiler1 code

--- src/main/java/com/github/xjln/compiler/Compiler.java
+++ src/main/java/com/github/xjln/compiler/Compiler.java
@@ -2,4 +2,25 @@ package com.github.xjln.compiler;
 
 public class Compiler {
 
+    public static String toDesc(String type){
+        return switch (type){
+            case "int" -> "I";
+            case "double" -> "D";
+            case "char" -> "C";
+            case "short" -> "S";
+            case "float" -> "F";
+            case "byte" -> "B";
+            case "boolean" -> "Z";
+            case "long" -> "J";
+            case "void" -> "V";
+            default -> "L" + type + ";";
+        };
+    }
+
+    public static String validateName(String name){
+        name = name.replace("/", ".");
+        name = name.replace("\\", ".");
+        //if(name.startsWith(srcFolder)) name = name.substring(srcFolder.length() + 1);
+        return name;
+    }
 }
--- src/main/java/com/github/xjln/compiler/Parser.java
+++ src/main/java/com/github/xjln/compiler/Parser.java
@@ -1,4 +1,51 @@
 package com.github.xjln.compiler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
 class Parser {
+    private final Set<String> primitives;
+    public final Lexer lexer;
+
+    private HashMap<String, String> uses;
+    //private XJLNClass current;
+    private String path;
+    private String className;
+    private Scanner sc;
+
+    public Parser(){
+        primitives = Set.of("int", "double", "long", "float", "boolean", "char", "byte", "short");
+        lexer = new Lexer();
+    }
+
+    public /*HashMap<String, XJLNClass>*/void parseFile(File file){
+        //HashMap<String, XJLNClass> classes = new HashMap<>();
+        uses = new HashMap<>();
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("file " + file.getPath() + " not found");
+        }
+
+        path = file.getPath();
+        path = path.substring(0, path.length() - 5);
+
+        String line;
+        while(sc.hasNextLine()){
+            line = sc.nextLine().trim();
+            if(!line.equals("") && !line.startsWith("#")){
+                if(line.startsWith("use")) parseUseDef(line);
+                else throw new RuntimeException("illegal argument in: " + line);
+            }
+        }
+
+        //return classes;
+    }
+
+    private void parseUseDef(String line){
+
+    }
 }
