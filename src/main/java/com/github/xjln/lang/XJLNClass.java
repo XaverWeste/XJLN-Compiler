911eaefef17e88da50fd636c4ed84b0b66aa988e
XaverWeste <westxave@kkg-do.eu>
2023-06-15 12:51:01 +0200

    XJLNMethod.java

--- src/main/java/com/github/xjln/lang/XJLNClass.java
+++ src/main/java/com/github/xjln/lang/XJLNClass.java
@@ -1,13 +1,17 @@
 package com.github.xjln.lang;
 
+import java.util.ArrayList;
+
 public class XJLNClass implements Compilable {
 
     public final String[] parameter;
     public final String constructor;
+    public final ArrayList<XJLNMethod> methods;
 
     public XJLNClass(String[] parameter, String constructor){
         this.parameter = parameter;
         this.constructor = constructor;
+        methods = new ArrayList<>();
     }
 
 }
---
+++ src/main/java/com/github/xjln/lang/XJLNMethod.java
@@ -0,0 +1,12 @@
+package com.github.xjln.lang;
+
+public class XJLNMethod {
+
+    public final String[] parameter;
+    public final String code;
+
+    public XJLNMethod(String[] parameter, String code){
+        this.parameter = parameter;
+        this.code = code;
+    }
+}
