15eb03854b63067d55e0e0a4f88b59c0763eb1a3
Niemand <90244583+XaverWeste@users.noreply.github.com>
2023-09-08 10:24:23 +0200

    Update XJLNField.java

--- src/main/java/com/github/xjln/lang/XJLNField.java
+++ src/main/java/com/github/xjln/lang/XJLNField.java
@@ -2,7 +2,7 @@ package com.github.xjln.lang;
 
 import java.lang.reflect.Field;
 
-public record XJLNField(boolean inner, boolean statik, boolean constant, String type, String name){
+public record XJLNField(boolean inner, boolean statik, boolean constant, String type, String name, String initValue){
 
     public static XJLNField ofField(Field field){
         return new XJLNField(field.toString().startsWith("private ") || field.toString().startsWith("protected "), field.toString().contains(" final "), field.toString().contains(" static "),field.getType().toString().split(" ", 2)[1], field.getName());
