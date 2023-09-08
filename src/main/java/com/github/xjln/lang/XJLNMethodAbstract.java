e67f9d35e04264714ab0fc3236ad1a23469661a5
Niemand <90244583+XaverWeste@users.noreply.github.com>
2023-09-08 10:32:26 +0200

    Update XJLNMethodAbstract.java

--- src/main/java/com/github/xjln/lang/XJLNMethodAbstract.java
+++ src/main/java/com/github/xjln/lang/XJLNMethodAbstract.java
@@ -21,8 +21,17 @@ public sealed class XJLNMethodAbstract permits XJLNMethod{
     public XJLNMethodAbstract(boolean inner, String name, MatchedList<String, String> parametetTypes, String returnType){
         this.inner = inner;
         this.name = name;
+        this.genericTypes = null;
         this.parameterTypes = parameterTypes;
         this.returnType = returnType;
+    }
+
+    public XJLNMethodAbstract(XJLNMethodAbstract am){
+        this.inner = am.inner:
+        this.name = am.name;
+        this.genericTypes = am.genericTypes;
+        this.parameterTypes = am.parameterTypes;
+        thus.returnType = am.returnType;
     }
 
     public XJLNMethod implementMethod(String[] code){
